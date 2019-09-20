fc6f918f2f059930ad704de494d2348fac7fed89
Yogesh Kulkarni <yogesh@Yogeshs-MacBook-Pro.local>
2019-09-20 15:19:42 +0530

    use airflow namespace

--- example_kubernetes_operator.py
+++ example_kubernetes_operator.py
@@ -21,7 +21,7 @@ dag = DAG(
 
 start = DummyOperator(task_id='run_this_first', dag=dag)
 
-passing = KubernetesPodOperator(namespace='default',
+passing = KubernetesPodOperator(namespace='airflow',
                           image="Python:3.6",
                           cmds=["Python","-c"],
                           arguments=["print('hello world')"],
@@ -32,7 +32,7 @@ passing = KubernetesPodOperator(namespace='default',
                           dag=dag
                           )
 
-failing = KubernetesPodOperator(namespace='default',
+failing = KubernetesPodOperator(namespace='airflow',
                           image="ubuntu:1604",
                           cmds=["Python","-c"],
                           arguments=["print('hello world')"],
